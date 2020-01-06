acbad9c1d2485341f4311d26f7247baef4cae817
Ethan Wams <infoptwo@gmail.com>
2020-01-06 10:15:52 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # datatable-integration-tutorial
 
+jQuery datatables is a very useful library for displaying data in a tabular form.
+However, Vue, and jQuery are fundamentally different in paradigm i.e. Vue is data first, so
+changes in the DOM are done by changing the data wheras jQuery manipulates the DOM directly
+regardless of the data.
+
+Hence the motivation of this project which contains a brief example of how to integrate jQuery Datatables with Vue while keeping the Vue data first paradigm, and preventing conflict between
+the changes that Vue makes in line with changes to data, and those made by the jQuery datatable.
+
+## Before we begin, kindly note the following:
+1. Note that this example has been tailored for Ubuntu users
+2. jQuery, and jQuery datatables have not been installed with npm but instead their scripts have been added to the index.html page for speed, and simplicity
+3. It's good practice to separate jQuery datatables into its own component to prevent the table from being overwritten by changes to Vue data
+
+## Prerequisites
+1. A functional internet connection
+2. Ubuntu version 16.04 and above
+3. npm
+4. Global Vue CLI
+
 ## Project setup
 ```
 npm install
@@ -10,15 +29,56 @@ npm install
 npm run serve
 ```
 
-### Compiles and minifies for production
+## Key steps
+Assuming you've created a basic vue project with using `vue create your-project-name-here`
+1. Remove the unnecessary hello world component, and modify the index html titles as you please
+2. Add the following scripts at the bottom of the body of the index.html page
+
 ```
-npm run build
+    <script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap.min.js"></script>
+```
+3. Create a datatable component with one `<table></table>` element as shown in the example below
+```
+<template>
+    <table id="sample-datatable" style="cursor:pointer;" class="table table-hover"></table> 
+</template>
+```
+4. Create the component boiler plate
+```
+export default {
+    name: 'my-component-name',
+    data(){
+        return {
+            columns: [],
+            rowData: []
+        }
+    },
+    mounted(){
+        // We'll initialize the datatable here
+    }
+}
+```
+5. Declare the columns, and data you'd like. For more information on how to do this using jQuery datatables, visit the [package documentation](https://datatables.net/examples/data_sources/js_array.html)
+6. Initialize the datatable, add the data, and draw it on the DOM
+```
+// Initialize the datatable and store it in dtHandle for easy access
+// throughout the component
+this.dtHandle = $(this.$el).DataTable({
+    columns: this.columns
+});
+
+// Add the row data
+this.dtHandle.rows.add(this.rowData)
+
+// Draw the table... Without this step, it won't be visible
+this.dtHandle.draw()
 ```
 
-### Lints and fixes files
-```
-npm run lint
-```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
---
+++ src/components/Datatable.vue
@@ -0,0 +1,630 @@
+<template>
+    <table id="sample-datatable" style="cursor:pointer;" class="table table-hover"></table> 
+</template>
+
+<script>
+export default {
+  name: 'Datatable',
+  data(){
+    return {
+      columns: [
+        {title: "First Name", data: "fname"},
+        {title: "Last Name", data: "lname"},
+        {title: "Date of Birth", data: "dob"},
+        {title: "Height (cm)", data: "height"}
+      ],
+      rowData: [
+        {
+          "fname": "Carter",
+          "lname": "Gentry",
+          "dob": "2009-05-11",
+          "height": 172
+        },
+        {
+          "fname": "Virginia",
+          "lname": "Carroll",
+          "dob": "2007-04-03",
+          "height": 170
+        },
+        {
+          "fname": "Mira",
+          "lname": "Contreras",
+          "dob": "2009-06-15",
+          "height": 167
+        },
+        {
+          "fname": "Yasir",
+          "lname": "Barron",
+          "dob": "2008-09-21",
+          "height": 165
+        },
+        {
+          "fname": "Yuli",
+          "lname": "Carney",
+          "dob": "2007-08-21",
+          "height": 165
+        },
+        {
+          "fname": "Willa",
+          "lname": "Zimmerman",
+          "dob": "2008-05-14",
+          "height": 168
+        },
+        {
+          "fname": "Alexandra",
+          "lname": "Cote",
+          "dob": "2009-06-28",
+          "height": 190
+        },
+        {
+          "fname": "Arthur",
+          "lname": "Tucker",
+          "dob": "2009-08-09",
+          "height": 184
+        },
+        {
+          "fname": "Nash",
+          "lname": "Grant",
+          "dob": "2008-10-29",
+          "height": 180
+        },
+        {
+          "fname": "Darryl",
+          "lname": "York",
+          "dob": "2009-03-13",
+          "height": 188
+        },
+        {
+          "fname": "Lucy",
+          "lname": "Rowe",
+          "dob": "2009-08-28",
+          "height": 161
+        },
+        {
+          "fname": "Brenden",
+          "lname": "Diaz",
+          "dob": "2009-06-10",
+          "height": 184
+        },
+        {
+          "fname": "Phoebe",
+          "lname": "Willis",
+          "dob": "2007-10-21",
+          "height": 171
+        },
+        {
+          "fname": "Leah",
+          "lname": "Sellers",
+          "dob": "2008-12-03",
+          "height": 162
+        },
+        {
+          "fname": "Henry",
+          "lname": "Tucker",
+          "dob": "2007-04-25",
+          "height": 162
+        },
+        {
+          "fname": "Nelle",
+          "lname": "Jimenez",
+          "dob": "2008-03-15",
+          "height": 165
+        },
+        {
+          "fname": "Hilel",
+          "lname": "Wooten",
+          "dob": "2009-06-06",
+          "height": 184
+        },
+        {
+          "fname": "Lucius",
+          "lname": "Sloan",
+          "dob": "2007-04-16",
+          "height": 184
+        },
+        {
+          "fname": "Shea",
+          "lname": "Mcmahon",
+          "dob": "2007-07-02",
+          "height": 160
+        },
+        {
+          "fname": "Neil",
+          "lname": "Bird",
+          "dob": "2008-11-06",
+          "height": 170
+        },
+        {
+          "fname": "Alice",
+          "lname": "Bass",
+          "dob": "2009-10-01",
+          "height": 168
+        },
+        {
+          "fname": "Benjamin",
+          "lname": "Dorsey",
+          "dob": "2009-08-18",
+          "height": 165
+        },
+        {
+          "fname": "Odessa",
+          "lname": "Sanchez",
+          "dob": "2009-06-21",
+          "height": 162
+        },
+        {
+          "fname": "Thor",
+          "lname": "Mccoy",
+          "dob": "2008-10-10",
+          "height": 174
+        },
+        {
+          "fname": "Cassandra",
+          "lname": "Whitley",
+          "dob": "2007-12-20",
+          "height": 168
+        },
+        {
+          "fname": "Britanni",
+          "lname": "Mack",
+          "dob": "2008-03-18",
+          "height": 163
+        },
+        {
+          "fname": "Tamekah",
+          "lname": "Haynes",
+          "dob": "2007-05-01",
+          "height": 183
+        },
+        {
+          "fname": "Boris",
+          "lname": "Branch",
+          "dob": "2009-03-16",
+          "height": 162
+        },
+        {
+          "fname": "Petra",
+          "lname": "Cooke",
+          "dob": "2007-01-20",
+          "height": 175
+        },
+        {
+          "fname": "Brianna",
+          "lname": "Fitzpatrick",
+          "dob": "2009-02-03",
+          "height": 173
+        },
+        {
+          "fname": "Aurelia",
+          "lname": "Cash",
+          "dob": "2009-03-04",
+          "height": 174
+        },
+        {
+          "fname": "Kareem",
+          "lname": "Pope",
+          "dob": "2007-08-01",
+          "height": 161
+        },
+        {
+          "fname": "Caryn",
+          "lname": "Copeland",
+          "dob": "2007-05-17",
+          "height": 167
+        },
+        {
+          "fname": "Ferdinand",
+          "lname": "Riggs",
+          "dob": "2008-10-03",
+          "height": 182
+        },
+        {
+          "fname": "Avye",
+          "lname": "Craig",
+          "dob": "2007-08-29",
+          "height": 169
+        },
+        {
+          "fname": "Jaquelyn",
+          "lname": "Hodges",
+          "dob": "2009-02-10",
+          "height": 174
+        },
+        {
+          "fname": "Declan",
+          "lname": "Clayton",
+          "dob": "2008-08-30",
+          "height": 169
+        },
+        {
+          "fname": "Kirby",
+          "lname": "Case",
+          "dob": "2009-09-27",
+          "height": 170
+        },
+        {
+          "fname": "Hayley",
+          "lname": "Briggs",
+          "dob": "2007-07-11",
+          "height": 182
+        },
+        {
+          "fname": "Mara",
+          "lname": "Wheeler",
+          "dob": "2008-11-30",
+          "height": 168
+        },
+        {
+          "fname": "Linus",
+          "lname": "Harmon",
+          "dob": "2009-10-05",
+          "height": 184
+        },
+        {
+          "fname": "Quin",
+          "lname": "Kirby",
+          "dob": "2008-03-06",
+          "height": 184
+        },
+        {
+          "fname": "Isabella",
+          "lname": "Boyle",
+          "dob": "2008-01-13",
+          "height": 182
+        },
+        {
+          "fname": "Tanner",
+          "lname": "Johnson",
+          "dob": "2009-07-16",
+          "height": 173
+        },
+        {
+          "fname": "Emmanuel",
+          "lname": "Atkinson",
+          "dob": "2007-09-18",
+          "height": 172
+        },
+        {
+          "fname": "Cade",
+          "lname": "Thornton",
+          "dob": "2008-03-11",
+          "height": 184
+        },
+        {
+          "fname": "Sharon",
+          "lname": "Bartlett",
+          "dob": "2008-07-22",
+          "height": 166
+        },
+        {
+          "fname": "Reece",
+          "lname": "Mullen",
+          "dob": "2009-06-24",
+          "height": 179
+        },
+        {
+          "fname": "Faith",
+          "lname": "Stevenson",
+          "dob": "2008-11-28",
+          "height": 165
+        },
+        {
+          "fname": "Chadwick",
+          "lname": "Dunn",
+          "dob": "2007-10-28",
+          "height": 184
+        },
+        {
+          "fname": "Laura",
+          "lname": "Bowman",
+          "dob": "2008-10-09",
+          "height": 187
+        },
+        {
+          "fname": "Kay",
+          "lname": "Berry",
+          "dob": "2009-02-11",
+          "height": 169
+        },
+        {
+          "fname": "Mason",
+          "lname": "Stokes",
+          "dob": "2008-12-24",
+          "height": 165
+        },
+        {
+          "fname": "Uta",
+          "lname": "Cohen",
+          "dob": "2009-04-05",
+          "height": 160
+        },
+        {
+          "fname": "Sonya",
+          "lname": "Austin",
+          "dob": "2008-07-16",
+          "height": 170
+        },
+        {
+          "fname": "Elijah",
+          "lname": "Fischer",
+          "dob": "2009-04-30",
+          "height": 168
+        },
+        {
+          "fname": "Evan",
+          "lname": "Kemp",
+          "dob": "2008-08-22",
+          "height": 172
+        },
+        {
+          "fname": "Wyatt",
+          "lname": "Whitfield",
+          "dob": "2007-05-29",
+          "height": 181
+        },
+        {
+          "fname": "Wayne",
+          "lname": "Montgomery",
+          "dob": "2009-11-17",
+          "height": 168
+        },
+        {
+          "fname": "Timothy",
+          "lname": "Joyner",
+          "dob": "2009-10-19",
+          "height": 161
+        },
+        {
+          "fname": "Juliet",
+          "lname": "Rocha",
+          "dob": "2007-11-28",
+          "height": 185
+        },
+        {
+          "fname": "Brooke",
+          "lname": "Murphy",
+          "dob": "2009-07-11",
+          "height": 190
+        },
+        {
+          "fname": "Vance",
+          "lname": "Fields",
+          "dob": "2008-05-15",
+          "height": 165
+        },
+        {
+          "fname": "Jacob",
+          "lname": "Johns",
+          "dob": "2007-08-01",
+          "height": 172
+        },
+        {
+          "fname": "Keith",
+          "lname": "Burris",
+          "dob": "2007-06-08",
+          "height": 181
+        },
+        {
+          "fname": "Miranda",
+          "lname": "Duffy",
+          "dob": "2007-03-20",
+          "height": 178
+        },
+        {
+          "fname": "Rashad",
+          "lname": "Buck",
+          "dob": "2008-02-02",
+          "height": 183
+        },
+        {
+          "fname": "Dominic",
+          "lname": "Woodward",
+          "dob": "2008-02-09",
+          "height": 187
+        },
+        {
+          "fname": "Nasim",
+          "lname": "Shelton",
+          "dob": "2008-09-14",
+          "height": 184
+        },
+        {
+          "fname": "MacKensie",
+          "lname": "Church",
+          "dob": "2008-06-23",
+          "height": 180
+        },
+        {
+          "fname": "Wang",
+          "lname": "Torres",
+          "dob": "2008-12-08",
+          "height": 171
+        },
+        {
+          "fname": "Anne",
+          "lname": "Mcdonald",
+          "dob": "2008-04-29",
+          "height": 176
+        },
+        {
+          "fname": "Kelly",
+          "lname": "Lawson",
+          "dob": "2008-10-24",
+          "height": 177
+        },
+        {
+          "fname": "Bradley",
+          "lname": "Vinson",
+          "dob": "2009-11-27",
+          "height": 186
+        },
+        {
+          "fname": "Michelle",
+          "lname": "Norman",
+          "dob": "2008-10-06",
+          "height": 163
+        },
+        {
+          "fname": "Edward",
+          "lname": "Eaton",
+          "dob": "2008-06-23",
+          "height": 173
+        },
+        {
+          "fname": "Lacy",
+          "lname": "Huff",
+          "dob": "2009-08-22",
+          "height": 161
+        },
+        {
+          "fname": "Xyla",
+          "lname": "Snow",
+          "dob": "2007-05-11",
+          "height": 163
+        },
+        {
+          "fname": "Hoyt",
+          "lname": "Benton",
+          "dob": "2007-01-21",
+          "height": 169
+        },
+        {
+          "fname": "Imani",
+          "lname": "Rocha",
+          "dob": "2007-03-25",
+          "height": 162
+        },
+        {
+          "fname": "Ashton",
+          "lname": "Palmer",
+          "dob": "2009-02-07",
+          "height": 189
+        },
+        {
+          "fname": "Thor",
+          "lname": "Page",
+          "dob": "2007-01-14",
+          "height": 181
+        },
+        {
+          "fname": "Echo",
+          "lname": "Carpenter",
+          "dob": "2007-03-29",
+          "height": 179
+        },
+        {
+          "fname": "George",
+          "lname": "Parker",
+          "dob": "2007-03-24",
+          "height": 176
+        },
+        {
+          "fname": "Kasper",
+          "lname": "Kemp",
+          "dob": "2009-11-20",
+          "height": 170
+        },
+        {
+          "fname": "Lydia",
+          "lname": "Poole",
+          "dob": "2007-03-03",
+          "height": 187
+        },
+        {
+          "fname": "Uma",
+          "lname": "Hansen",
+          "dob": "2009-08-12",
+          "height": 171
+        },
+        {
+          "fname": "Oren",
+          "lname": "Scott",
+          "dob": "2007-12-16",
+          "height": 166
+        },
+        {
+          "fname": "Camilla",
+          "lname": "Jimenez",
+          "dob": "2008-05-06",
+          "height": 173
+        },
+        {
+          "fname": "Rachel",
+          "lname": "Harding",
+          "dob": "2008-12-24",
+          "height": 169
+        },
+        {
+          "fname": "Quyn",
+          "lname": "Valencia",
+          "dob": "2009-06-09",
+          "height": 170
+        },
+        {
+          "fname": "Stacey",
+          "lname": "Fitzpatrick",
+          "dob": "2007-08-14",
+          "height": 162
+        },
+        {
+          "fname": "Quinn",
+          "lname": "Richard",
+          "dob": "2009-11-07",
+          "height": 185
+        },
+        {
+          "fname": "Rowan",
+          "lname": "Glover",
+          "dob": "2007-01-27",
+          "height": 184
+        },
+        {
+          "fname": "Sage",
+          "lname": "Maddox",
+          "dob": "2007-01-23",
+          "height": 168
+        },
+        {
+          "fname": "Hayfa",
+          "lname": "Snyder",
+          "dob": "2008-07-11",
+          "height": 175
+        },
+        {
+          "fname": "Melissa",
+          "lname": "Herrera",
+          "dob": "2009-10-02",
+          "height": 177
+        },
+        {
+          "fname": "Octavius",
+          "lname": "Moody",
+          "dob": "2009-10-15",
+          "height": 182
+        },
+        {
+          "fname": "Aurora",
+          "lname": "Jennings",
+          "dob": "2009-03-15",
+          "height": 186
+        },
+        {
+          "fname": "Lucius",
+          "lname": "Mitchell",
+          "dob": "2008-05-01",
+          "height": 173
+        }
+      ]
+    }
+  },
+  mounted(){
+    this.dtHandle = $(this.$el).DataTable({
+      columns: this.columns
+    });
+
+    this.dtHandle.rows.add(this.rowData)
+
+    this.dtHandle.draw()
+  }
+}
+</script>
